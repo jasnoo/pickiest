af87de05290fb23a3905be985daa61a1d36faf08
Jasmine <jasminee@gmail.com>
2023-05-29 17:02:01 -0400

    updated toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Switch from './components/Switch'
-
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Switch from "./components/Switch";
 
 export default function App() {
-  const [formName, setFormName] = useState<undefine | string>()
-  const [names, setNames] = useState<string[]>([])
-  const [isPerson, setIsPerson] = useState(true)
-  
-  function handleSubmit(e){
-    e.preventDefault()
-    const newNames = [...names,formName]
-    setNames(newNames)
-    setFormName('')
+  const [formName, setFormName] = useState<undefined | string>();
+  const [names, setNames] = useState<string[]>([]);
+  const [isPerson, setIsPerson] = useState(true);
+  const [count, setCount] = useState<number>(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newNames = [...names, formName];
+    setNames(newNames);
+    setFormName("");
   }
 
-  function toggle(){
-
+  function toggleIsPerson() {
+    console.log("this is happening?");
+    isPerson ? setIsPerson(false) : setIsPerson(true);
   }
 
   return (
     <>
-    <Header/>
-    
-     {/* Form to add names */}
-    <form onSubmit={handleSubmit}>
-    <label htmlFor='inputName'>Name:</label>
-    <input type='text' name='inputName' value={formName} required maxLength={20} onChange={e=>setFormName(e.target.value)}/>
-    <button type='submit' onSubmit={e=> handleSubmit(e)}>Add</button>
-    </form>
+      <Header />
 
-    <Switch/>
-    
-       
+      <Switch handleToggle={toggleIsPerson} />
 
- 
+      <button onClick={() => setCount(count - 1)}>-</button>
 
+      <input
+        type="number"
+        name="count"
+        value={count}
+        required
+        maxLength={1}
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+
+      {/* Form to add names */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="inputName">Name:</label>
+        <input
+          type="text"
+          name="inputName"
+          value={formName}
+          required
+          maxLength={20}
+          onChange={(e) => setFormName(e.target.value)}
+        />
+        <button type="submit" onSubmit={(e) => handleSubmit(e)}>
+          Add
+        </button>
+      </form>
     </>
-  )
+  );
 }
--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,37 +1,36 @@
 import SwitchSelector from "react-switch-selector";
 
-export default function Switch(){
-    const options = [
-   {
-       label: <span>Person</span>,
-       value: {
-            person: true
-       },
-       selectedBackgroundColor: "#0097e6",
-   },
-   {
-       label: "Groups",
-       value: "groups",
-       selectedBackgroundColor: "#fbc531"
-   }
-];
+export default function Switch(props) {
+  const options = [
+    {
+      label: <span>Person</span>,
+      value: {
+        person: true,
+      },
+      selectedBackgroundColor: "#0097e6",
+    },
+    {
+      label: "Groups",
+      value: "groups",
+      selectedBackgroundColor: "#fbc531",
+    },
+  ];
 
-const onChange = (newValue) => {
-    console.log(newValue);
-};
+  const onChange = () => props.handleToggle();
 
-const initialSelectedIndex = options.findIndex(({value}) => value === "groups");
+  const initialSelectedIndex = options.findIndex(
+    ({ value }) => value === "person"
+  );
 
-return (
-    <div className="your-required-wrapper" >
-        <SwitchSelector
-            onChange={onChange}
-            options={options}
-            initialSelectedIndex={initialSelectedIndex}
-            backgroundColor={"#353b48"}
-            fontColor={"#f5f6fa"}
-        />
+  return (
+    <div className="your-required-wrapper" style={{ width: 200, height: 40 }}>
+      <SwitchSelector
+        onChange={onChange}
+        options={options}
+        initialSelectedIndex={initialSelectedIndex}
+        backgroundColor={"#353b48"}
+        fontColor={"#f5f6fa"}
+      />
     </div>
-);
-
+  );
 }
