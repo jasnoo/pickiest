64cf28da406462d1d9345e7896ef92149c53e460
Jasmine <jasminee@gmail.com>
2023-06-20 18:14:46 -0400

    changed footer behavior

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <div className="footer">Made by <a href="https://github.com/jasnoo/pickiest">Jasmine N.</a></div>
+
+    )
+};
+
+export default Footer;
