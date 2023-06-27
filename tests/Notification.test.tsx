fc9759cdc994f17a2b788e1c6cb2805e8a46b582
Jasmine <jasminee@gmail.com>
2023-06-27 15:04:12 -0400

    updated

--- tests/Notification.test.tsx
+++ tests/Notification.test.tsx
@@ -3,7 +3,6 @@ import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Notification, { NotificationProps } from '../src/components/Notification'
 
-
 const notificationProps: NotificationProps = {
     message: 'Test Message'
 }
@@ -13,7 +12,6 @@ describe('Notification Component', () => {
     it('Correctly displays non-null message', () => {
         render(<Notification {...notificationProps} />)
         expect(screen.getByText('Test Message')).toBeInTheDocument()
-
     })
 
     it('Displays nothing when there is no message', () => {
